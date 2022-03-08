143c912687e38a21b01eb7e54c4e68ea035b63ed
Berenguer Pou <berenguer.pou@gmail.com>
2022-03-08 18:45:36 +0100

    add layout component / tested

--- __tests__/smoke.test.ts
+++
@@ -1,5 +0,0 @@
-describe("Given an empty smoky test", () => {
-  describe("When it recives nothing", () => {
-    test("Then it should show if covergae is properly configurated", () => {});
-  });
-});
