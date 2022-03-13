b45594c1180726fda62d371dab0f935f99e93056
Berenguer Pou <berenguer.pou@gmail.com>
2022-03-13 13:02:41 +0100

    changed name of test for HomePage to avoid error in deploy

--- src/pages/homePage.test.tsx
+++ src/pages/homePage.test.tsx
@@ -1,7 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import { Provider } from "react-redux";
 import store from "../redux/store";
-import HomePage from "./";
+import HomePage from ".";
 
 describe("Given a HomePage component", () => {
   describe("When it's rendered", () => {
