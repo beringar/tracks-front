cf112d5841bfd12f9459b68b1edbd35b337d27f1
Berenguer Pou <berenguer.pou@gmail.com>
2022-03-13 20:32:25 +0100

    add components MyTracksList and MyTracksItem

---
+++ src/components/MyTracksItem/MyTracksItem.tsx
@@ -0,0 +1,62 @@
+import {
+  Text,
+  ListItem,
+  IconButton,
+  HStack,
+  Flex,
+  Box,
+} from "@chakra-ui/react";
+import { FiEdit, FiEye, FiTrash2 } from "react-icons/fi";
+import styled from "@emotion/styled";
+
+const TrackItemContainer = styled(Flex)`
+  justify-content: space-between;
+  align-items: center;
+  @media only screen and (max-width: 767px) {
+    flex-direction: column;
+  }
+`;
+
+interface MyTracksItemProps {
+  name: string;
+  id: string;
+}
+
+const MyTracksItem = ({ name, id }: MyTracksItemProps): JSX.Element => {
+  const onClickDelete = () => {
+    console.log(id);
+  };
+
+  return (
+    <ListItem>
+      <Box p={5} shadow="md" borderWidth="1px" borderRadius="lg">
+        <TrackItemContainer>
+          <Text my={2}>{name}</Text>
+          <HStack spacing="10px">
+            <IconButton
+              colorScheme="green"
+              aria-label="View Track"
+              icon={<FiEye />}
+              isRound
+            />
+            <IconButton
+              colorScheme="yellow"
+              aria-label="Edit track"
+              icon={<FiEdit />}
+              isRound
+            />
+            <IconButton
+              colorScheme="red"
+              aria-label="Remove track"
+              icon={<FiTrash2 />}
+              isRound
+              onClick={onClickDelete}
+            />
+          </HStack>
+        </TrackItemContainer>
+      </Box>
+    </ListItem>
+  );
+};
+
+export default MyTracksItem;
--- src/pages/my-profile/index.tsx
+++ src/pages/my-profile/index.tsx
@@ -1,27 +1,37 @@
 import type { NextPage } from "next";
-import {
-  Heading,
-  Container,
-  Text,
-  List,
-  ListItem,
-  ListIcon,
-  Divider,
-  IconButton,
-  HStack,
-  Flex,
-  Box,
-} from "@chakra-ui/react";
-import { FiMapPin, FiEdit, FiEye, FiTrash2 } from "react-icons/fi";
-import styled from "@emotion/styled";
+import { Heading } from "@chakra-ui/react";
+import MyTracksList from "../../components/MyTracksList/MyTracksList";
+import { ITrack } from "../../types/ITrack";
 
-const TrackItemContainer = styled(Flex)`
-  justify-content: space-between;
-  align-items: center;
-  @media only screen and (max-width: 767px) {
-    flex-direction: column;
-  }
-`;
+const tracks: ITrack[] = [
+  {
+    name: "Tuc de Sendrós per llac de Saboredo",
+    refuge: "Saboredo",
+    difficulty: "normal",
+    kids: true,
+    seasons: ["spring", "summer"],
+    description:
+      "Description of track, this route is very appealing because...",
+    image: "https://mapio.net/images-p/7224428.jpg",
+    gpx: "http://www.apatita.com/gps/aiguestortes_2_amitges_saboredo_colomers.zip",
+    user: "6228d9e2d3b484d4871608ee",
+    id: "6229bdbccf53a1fa6ac36821",
+  },
+  {
+    name: "Punta Alta de Comalesbienes des de Cavallers",
+    refuge: "Ventosa i Calvell",
+    difficulty: "high",
+    kids: false,
+    seasons: ["spring", "summer"],
+    description:
+      "Description of track, this route is very appealing because...",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/e/ea/P1280563x_-_Pic_de_Comalesbienes.JPG",
+    gpx: "http://www.apatita.com/gps/aiguestortes_2_amitges_saboredo_colomers.zip",
+    user: "6228d9e2d3b484d4871608ee",
+    id: "6229c2a2cf53a1fa6ac36823",
+  },
+];
 
 const MyProfilePage: NextPage = () => {
   return (
@@ -29,95 +39,7 @@ const MyProfilePage: NextPage = () => {
       <Heading as="h1" size="md" mb={3}>
         My Tracks
       </Heading>
-      <List spacing={3}>
-        <ListItem>
-          <Box p={5} shadow="md" borderWidth="1px" borderRadius="lg">
-            <TrackItemContainer>
-              <Text my={2}>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit
-              </Text>
-              <HStack spacing="10px">
-                <IconButton
-                  colorScheme="green"
-                  aria-label="View Track"
-                  icon={<FiEye />}
-                  isRound
-                />
-                <IconButton
-                  colorScheme="yellow"
-                  aria-label="Edit track"
-                  icon={<FiEdit />}
-                  isRound
-                />
-                <IconButton
-                  colorScheme="red"
-                  aria-label="Remove track"
-                  icon={<FiTrash2 />}
-                  isRound
-                />
-              </HStack>
-            </TrackItemContainer>
-          </Box>
-        </ListItem>
-        <ListItem>
-          <Box p={5} shadow="md" borderWidth="1px" borderRadius="lg">
-            <TrackItemContainer>
-              <Text my={2}>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit
-              </Text>
-              <HStack spacing="10px">
-                <IconButton
-                  colorScheme="green"
-                  aria-label="View Track"
-                  icon={<FiEye />}
-                  isRound
-                />
-                <IconButton
-                  colorScheme="yellow"
-                  aria-label="Edit track"
-                  icon={<FiEdit />}
-                  isRound
-                />
-                <IconButton
-                  colorScheme="red"
-                  aria-label="Remove track"
-                  icon={<FiTrash2 />}
-                  isRound
-                />
-              </HStack>
-            </TrackItemContainer>
-          </Box>
-        </ListItem>
-        <ListItem>
-          <Box p={5} shadow="md" borderWidth="1px" borderRadius="lg">
-            <TrackItemContainer>
-              <Text my={2}>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit
-              </Text>
-              <HStack spacing="10px">
-                <IconButton
-                  colorScheme="green"
-                  aria-label="View Track"
-                  icon={<FiEye />}
-                  isRound
-                />
-                <IconButton
-                  colorScheme="yellow"
-                  aria-label="Edit track"
-                  icon={<FiEdit />}
-                  isRound
-                />
-                <IconButton
-                  colorScheme="red"
-                  aria-label="Remove track"
-                  icon={<FiTrash2 />}
-                  isRound
-                />
-              </HStack>
-            </TrackItemContainer>
-          </Box>
-        </ListItem>
-      </List>
+      <MyTracksList tracks={tracks} />
     </>
   );
 };
