9d4192d7e5a79e0bfc8fd4fc60d93334c97136af
Berenguer Pou <berenguer.pou@gmail.com>
2022-03-15 16:13:39 +0100

    add wrapper functionality to MyApp in /pages/_app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,25 +2,22 @@ import "@fontsource/inter/400.css";
 import "@fontsource/inter/700.css";
 import "@fontsource/montserrat-alternates/400.css";
 import "@fontsource/montserrat-alternates/700.css";
-import { Provider } from "react-redux";
-import store from "../redux/store";
 import { ChakraProvider, ScaleFade } from "@chakra-ui/react";
 import theme from "../theme";
 import type { AppProps } from "next/app";
 import Layout from "../components/Layout/Layout";
+import { wrapper } from "../redux/store";
 
 function MyApp({ Component, pageProps, router }: AppProps) {
   return (
-    <Provider store={store}>
-      <ChakraProvider resetCSS theme={theme}>
-        <Layout>
-          <ScaleFade key={router.route} initialScale={0.9} in={true}>
-            <Component {...pageProps} />
-          </ScaleFade>
-        </Layout>
-      </ChakraProvider>
-    </Provider>
+    <ChakraProvider resetCSS theme={theme}>
+      <Layout>
+        <ScaleFade key={router.route} initialScale={0.9} in={true}>
+          <Component {...pageProps} />
+        </ScaleFade>
+      </Layout>
+    </ChakraProvider>
   );
 }
 
-export default MyApp;
+export default wrapper.withRedux(MyApp);
