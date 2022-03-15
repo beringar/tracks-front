87f3f71647f7f6cb77b341be5b0d5c20232fcd31
Berenguer Pou <berenguer.pou@gmail.com>
2022-03-15 16:09:32 +0100

    changed dispatch dfrom redux to dispatch exported from /store

--- src/redux/thunks/tracksThunks.ts
+++ src/redux/thunks/tracksThunks.ts
@@ -1,29 +1,27 @@
-import { Dispatch } from "react";
-import { ILoadAllTracksAction } from "../../types/IAction";
+import { AppDispatch } from "../store";
 import { loadAllTracksAction } from "../actions/tracksActionCreator/tracksActionCreator";
 
 const APIUrl: string = process.env.NEXT_PUBLIC_TRACKS_API_URL;
 
-export const loadAllTracksThunk =
-  (toast) => async (dispatch: Dispatch<ILoadAllTracksAction>) => {
-    try {
-      const response = await fetch(`${APIUrl}tracks`);
-      const tracksAPI = await response.json();
-      if (!response.ok) {
-        toast({
-          title: "Error!",
-          description: tracksAPI.message,
-          status: "error",
-        });
-        return;
-      }
-      dispatch(loadAllTracksAction(tracksAPI.tracks));
-    } catch (error) {
+export const loadAllTracksThunk = (toast) => async (dispatch: AppDispatch) => {
+  try {
+    const response = await fetch(`${APIUrl}tracks`);
+    const tracksAPI = await response.json();
+    if (!response.ok) {
       toast({
-        position: "top",
-        title: "error!",
-        description: error.message,
+        title: "Error!",
+        description: tracksAPI.message,
         status: "error",
       });
+      return;
     }
-  };
+    dispatch(loadAllTracksAction(tracksAPI.tracks));
+  } catch (error) {
+    toast({
+      position: "top",
+      title: "error!",
+      description: error.message,
+      status: "error",
+    });
+  }
+};
