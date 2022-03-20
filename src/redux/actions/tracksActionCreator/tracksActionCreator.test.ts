f1fb6a7c27937b1d0452b98a61fc8d8543106f67
Berenguer Pou <berenguer.pou@gmail.com>
2022-03-20 15:56:54 +0100

    add test for createTrackAction

--- src/redux/actions/tracksActionCreator/tracksActionCreator.test.ts
+++ src/redux/actions/tracksActionCreator/tracksActionCreator.test.ts
@@ -1,10 +1,15 @@
-import { deleteTrackAction, loadAllTracksAction } from "./tracksActionCreator";
+import {
+  createTrackAction,
+  deleteTrackAction,
+  loadAllTracksAction,
+} from "./tracksActionCreator";
 import actionTypes from "../actionTypes";
+import { Track } from "../../../types/Track";
 
 describe("Given a loadAllTracks action", () => {
   describe("When it's called with an array of tracks", () => {
     test("Then it should return an object with the action type and the array", () => {
-      const tracks = [
+      const tracks: Track[] = [
         {
           name: "Tuc de Sendrós per llac de Saboredo",
           refuge: "Saboredo",
@@ -36,7 +41,7 @@ describe("Given a loadAllTracks action", () => {
 describe("Given a deleteTrackAction", () => {
   describe("When it receives an id", () => {
     test("Then it should return an action delete", () => {
-      const tracks = [
+      const tracks: Track[] = [
         {
           name: "Tuc de Sendrós per llac de Saboredo",
           refuge: "Saboredo",
@@ -58,6 +63,33 @@ describe("Given a deleteTrackAction", () => {
         id: "6229bdbccf53a1fa6ac36821",
       };
       const action = deleteTrackAction(tracks[0].id);
+      expect(action).toEqual(expectedAction);
+    });
+  });
+});
+
+describe("Given a createTrackAction", () => {
+  describe("When it receives a track", () => {
+    test("Then it should return an action delete", () => {
+      const track: Track = {
+        name: "Tuc de Sendrós per llac de Saboredo",
+        refuge: "Saboredo",
+        difficulty: "normal",
+        kids: true,
+        seasons: ["spring", "summer"],
+        description:
+          "Description of track, this route is very appealing because...",
+        image: "https://mapio.net/images-p/7224428.jpg",
+        gpx: "http://www.apatita.com/gps/aiguestortes_2_amitges_saboredo_colomers.zip",
+        user: "6228d9e2d3b484d4871608ee",
+        id: "6229bdbccf53a1fa6ac36821",
+        createdAt: "datefake",
+        updatedAt: "datefake",
+      };
+      const expectedAction = { type: "create-track", track };
+
+      const action = createTrackAction(track);
+
       expect(action).toEqual(expectedAction);
     });
   });
