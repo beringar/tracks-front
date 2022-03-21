ccf80e7b7adb21567245365aff756ed1eba0c948
Berenguer Pou <berenguer.pou@gmail.com>
2022-03-21 12:14:12 +0100

    add loginUser action type

--- src/redux/actions/actionTypes.ts
+++ src/redux/actions/actionTypes.ts
@@ -5,6 +5,7 @@ const actionTypes = {
   setSubmitting: "set-submit",
   unsetSubmitting: "unset-submitting",
   loadTrack: "load-track",
+  loginUser: "login-user",
 };
 
 export default actionTypes;
